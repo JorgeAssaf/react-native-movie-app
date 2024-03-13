584a22d827a4e53a4561eabc859d573ea3869e0f
Jorge Assaf <jorgeassaf160@gmail.com>
2024-03-13 10:55:52 -0600

    Refactor file structure and add navigation

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { StackNavigator } from './presentation/navigation/StackNavigator';
+
+export const App = () => {
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
+};
